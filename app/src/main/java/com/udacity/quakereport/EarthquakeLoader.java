dea23f9e2d6341a6d80aaef6d85b5a5e508e18e3
djpeneluppi <djpeneluppi@gmail.com>
2020-06-19 15:13:54 -0300

    EarthquakeLoader methods

--- app/src/main/java/com/udacity/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/udacity/quakereport/EarthquakeLoader.java
@@ -9,13 +9,43 @@ import androidx.loader.content.AsyncTaskLoader;
 import java.util.List;
 
 public class EarthquakeLoader extends AsyncTaskLoader<List<EarthQuake>> {
-    public EarthquakeLoader(@NonNull Context context) {
+
+    /** Tag para mensagens de log */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** URL da busca */
+    private String mUrl;
+
+    /**
+     * Constrói um novo {@link EarthquakeLoader}.
+     *
+     * @param context O contexto da activity
+     * @param url A URL de onde carregaremos dados
+     */
+    public EarthquakeLoader(Context context, String url) {
         super(context);
+        mUrl = url;
     }
 
-    @Nullable
     @Override
+    public void onStartLoading() {
+        forceLoad();
+    }
+
+
+/**
+ * Está e uma thread de background.
+ */    @Override
     public List<EarthQuake> loadInBackground() {
-        return null;
+        if(mUrl == null) {
+             return null;
+        }
+        // Realiza requisição de rede, decodifica a resposta,
+        // e extrai uma lista de earthquakes.
+        //List<EarthQuake> earthQuakes =
+
+
+    return null;
+
     }
 }
